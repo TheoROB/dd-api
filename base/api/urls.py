2cbdfcb4e2f6e6a47b9c72aa09a8fb31f94eeab7
paul-brd02 <paul.bardou@live.fr>
2023-01-26 23:45:07 +0100

    route working

--- base/api/urls.py
+++ base/api/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
 from . import views
-from .views import MyTokenObtainPairView
+# from .views import MyTokenObtainPairView
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
 
 urlpatterns = [
     path('', views.getRoutes),
+    path('students/', views.allStudents),
+    path('student/id/', views.studentID),
+    path('students/popularity', views.orderStudentsByPopularity,)
 
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 ]
--- base/api/views.py
+++ base/api/views.py
@@ -1,35 +1,48 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.http import HttpResponse
+
+from base.models import Player
 
 
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
+# from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+# from rest_framework_simplejwt.views import TokenObtainPairView
 
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
+# class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+#     @classmethod
+#     def get_token(cls, user):
+#         token = super().get_token(user)
 
-        # Add custom claims
-        token['username'] = user.username
-        # ...
+#         # Add custom claims
+#         token['username'] = user.username
+#         # ...
 
-        return token
+#         return token
 
 
-class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
+# class MyTokenObtainPairView(TokenObtainPairView):
+#     serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
-        '/api/token',
-        '/api/token/refresh',
+        # '/api/token',
+        # '/api/token/refresh',
         '/api/students',
         '/api/students/id',
-        '/api/students/popularite'
+        '/api/students/popularity'
     ]
 
     return Response(routes)
+
+def allStudents(request):
+    students = Player.objects.all()
+    return HttpResponse(students)
+
+def studentID(request):
+    return HttpResponse("ID de l'étudiant")
+
+def orderStudentsByPopularity(request):
+    return HttpResponse("Classement des 2tudiants par popularité")
