8ff2fc2fa241ee8d92ed3dd5e155f7fca4ab9ad2
paul-brd02 <paul.bardou@live.fr>
2023-01-26 10:41:29 +0100

    back ok

--- ddev_backend/ddev/ulrs.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from ddev.views import *
-
-router = routers.DefaultRouter()
-router.register('player', PlayerViewSet)
-router.register('user', UserViewSet)
-router.register('result', ResultViewSet)
