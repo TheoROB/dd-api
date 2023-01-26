19376a96b88f448da96fd34e6d119965b93d588d
pcazin <pablocazin.dev@gmail.com>
2023-01-26 22:49:59 +0100

    tables OK

--- base/models.py
+++ base/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    pseudo = models.CharField(max_length=30)
+    image = models.TextField()
+    elo = models.IntegerField() 
+    attack = models.IntegerField()
+    creator_id = models.IntegerField()
+
+class User(models.Model):
+  id = models.PositiveIntegerField(primary_key=True)
+  name = models.CharField(max_length=30)
+  password = models.CharField(max_length=30)
+
+class Results(models.Model):
+  player1_id = models.PositiveIntegerField()
+  player2_id = models.PositiveIntegerField()
+  winner_id = models.PositiveIntegerField()
