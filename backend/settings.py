e21d56e473d099b10c42bcb06e042038ad02aa4f
paul-brd02 <paul.bardou@live.fr>
2023-01-26 21:46:28 +0100

    DB postgresql

--- backend/settings.py
+++ backend/settings.py
@@ -121,8 +121,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DDEV',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5433', 
     }
 }
 
